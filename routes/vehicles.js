f6b3560a3d5c0ce6d9d0bd30a12bae84cee66011
Matthias Jäggli <matthias.jaeggli@scout24.ch>
2017-05-03 14:03:23 +0200

    initial question is also a next question

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,17 +1,25 @@
 const { send } = require('../views/response')
 const vehicles = require('../data/vehicles.json')
 const questions = require('../data/questions.json')
+const coredata = require('../data/coredata.json')
 
-const getQuestion = id => questions.filter(question => question.id === id).pop()
-const fillAnswers = (id, values) => {
-  const question = Object.assign({}, getQuestion(id))
-  question.answers = question.answers.map((answer, i) => {
-    values[i].forEach(value => {
-      answer.desc = answer.desc.replace('%d', value)
-      answer.value = answer.value.replace('%d', value)
+const getQuestion = stub => {
+  const question = Object.assign({}, questions.filter(question => question.id === stub.id).pop())
+  if (question.type === 'autocomplete' && question.answers === null) {
+    var data = coredata[question.id]
+    question.answers = Object.keys(data).map(key => ({
+      title: data[key],
+      value: key
+    }))
+  } else if (question.type === 'numeric') {
+    question.answers = question.answers.map((answer, i) => {
+      stub.values[i].forEach(value => {
+        answer.desc = answer.desc.replace('%d', value)
+        answer.value = answer.value.replace('%d', value)
+      })
+      return answer
     })
-    return answer
-  })
+  }
   return question
 }
 
@@ -19,11 +27,21 @@ module.exports = (req, res, next) => {
   let data = vehicles
   const page = 1
   const perPage = 100
+  const nextQuestion = {
+    id: 'MakeId'
+  }
 
   // make filter
   if (req.query.MakeId) {
     var make = parseInt(req.query.MakeId, 10)
     data = data.filter(vehicle => vehicle.MakeId === make)
+    nextQuestion.id = 'Price'
+    nextQuestion.values = [
+      [5000],
+      [5000, 12000],
+      [12000, 18000],
+      [18000]
+    ]
   }
 
   send(res, {
@@ -33,11 +51,6 @@ module.exports = (req, res, next) => {
       page,
       perPage
     },
-    next: fillAnswers('Price', [
-      [5000],
-      [5000, 12000],
-      [12000, 18000],
-      [18000]
-    ])
+    next: getQuestion(nextQuestion)
   })
 }
