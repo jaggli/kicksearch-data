66c51dfc3ad02504a325bf657bdc914b2b6e4af2
Matthias Jäggli <matthias.jaeggli@scout24.ch>
2017-05-03 11:43:04 +0200

    filterable coredata and filters

--- routes/filters.js
+++ routes/filters.js
@@ -2,5 +2,14 @@ const { send } = require('../views/response')
 const data = require('../data/filters.json')
 
 module.exports = (req, res, next) => {
-  send(res, data)
+  let ret = {}
+  const filter = req.query.filter || ''
+  let found = false
+  filter.split(',').forEach(key => {
+    if (!data[key]) { return }
+    found = true
+    ret[key] = data[key]
+  })
+  if (!found) { ret = data }
+  send(res, ret)
 }
