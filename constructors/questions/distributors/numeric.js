dbae9a98c89ecff4d6907c28f7bd8faff4bd69f4
Matthias Jäggli <matthias.jaeggli@scout24.ch>
2017-05-04 14:21:19 +0200

    renmove zero

--- constructors/questions/distributors/numeric.js
+++ constructors/questions/distributors/numeric.js
@@ -3,7 +3,9 @@ const math = require('mathjs')
 module.exports = (vehicles, keys) => {
   const data = {}
   keys.forEach(key => {
-    const values = vehicles.map(vehicle => vehicle[key]).sort()
+    const values = vehicles.map(vehicle => vehicle[key])
+      .filter(value => value > 0)
+      .sort()
     const median = math.median(values)
     let medianIndex = Math.round(values.length / 2)
 
@@ -14,15 +16,18 @@ module.exports = (vehicles, keys) => {
       medianIndex++
     }
 
-    const lower = math.median(values.slice(0, medianIndex))
-    const upper = math.median(values.slice(medianIndex))
+    const lowerHalf = values.slice(0, medianIndex)
+    const upperHalf = values.slice(medianIndex)
+
+    const lowerMedian = lowerHalf.length ? math.median(lowerHalf) : 0
+    const upperMedian = upperHalf.length ? math.median(upperHalf) : 0
 
     data[key] = {
       values: [
-        [lower],
-        [lower, median],
-        [median, upper],
-        [upper]
+        [lowerMedian],
+        [lowerMedian, median],
+        [median, upperMedian],
+        [upperMedian]
       ]
     }
   })
