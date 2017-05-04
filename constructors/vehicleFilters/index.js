e6eb46dda5ddf3f89937a8d407cff9c275e16223
Matthias Jäggli <matthias.jaeggli@scout24.ch>
2017-05-04 08:36:49 +0200

    dynamically generate filteres

--- constructors/vehicleFilters/index.js
+++ constructors/vehicleFilters/index.js
@@ -1,32 +1,21 @@
-const oneof = require('./oneof')
+const questions = require('../../data/questions.json')
+const distinct = require('./distinct')
 const numeric = require('./numeric')
 
 const noop = vehicles => vehicles
 
-const filters = {
-  MakeId: oneof('MakeId'),
-  ModelId: oneof('ModelId'),
-  FirstRegYear: numeric('FirstRegYear'),
-  BodyTypeId: oneof('BodyTypeId'),
-  FuelTypeId: oneof('FuelTypeId'),
-  TransmissionTypeId: oneof('TransmissionTypeId'),
-  ConsumptionRatingTypeId: oneof('ConsumptionRatingTypeId'),
-  ConsumptionTotal: numeric('ConsumptionTotal'),
-  Doors: numeric('Doors'),
-  Seats: numeric('Seats'),
-  Ccm: numeric('Ccm'),
-  Hp: numeric('Hp'),
-  BodyColorId: oneof('BodyColorId'),
-  Price: numeric('Price'),
-  Co2Emission: numeric('Co2Emission'),
-  Km: numeric('Km'),
-  InteriorColorId: oneof('InteriorColorId'),
-  Cylinders: numeric('Cylinders'),
-  Gears: numeric('Gears'),
-  WeightTotal: numeric('WeightTotal'),
-  IsAccidented: noop,
-  Equipment: noop
+const typeHandler = {
+  autocomplete: distinct,
+  distinct,
+  numeric,
+  boolean: noop,
+  array: noop
 }
+
+const filters = questions.reduce((ret, item) => {
+  ret[item.id] = typeHandler[item.type]
+  return ret
+}, {})
 
 module.exports = {
   applyQuery: (vehicles, query) => Object.keys(query || {})
--- constructors/vehicleFilters/make.js
+++
@@ -1,5 +0,0 @@
-module.exports = (vehicles, value) => {
-  if (!value) { return vehicles }
-  var make = parseInt(value, 10)
-  return vehicles.filter(vehicle => vehicle.MakeId === make)
-}
